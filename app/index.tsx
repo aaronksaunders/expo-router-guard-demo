db93bbac6deaa96f7a053d2f53c3e30894794cb9
aaron saunders <aaronksaunders@gmail.com>
2025-10-14 22:50:54 -0400

    step-1

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Text>Open up app/_layout.tsx to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
